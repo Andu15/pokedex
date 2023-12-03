fa6bca3ad4a149d3ac5c7459eb47d0192709f26a
Andu15 <estefania_8_3@hotmail.com>
2023-12-02 21:30:41 -0500

    delete vuex files

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
 import '@/assets/sass/main.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
